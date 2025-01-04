61143ef786203536e35bb22da1a207aa905a639d
AI-MOO <eemoh.osman@gmail.com>
2025-01-04 07:28:30 +0300

    Operation description update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from enum import Enum 
+from typing import Optional
+from fastapi import status, Response 
 # fastapi object 
 app = FastAPI()
 
@@ -14,10 +16,40 @@ class BlogType(str, Enum):
 def index():
     return {'message': 'Hello World !!!'}
 
-@app.get('/blog/all')
-def get_all_blogs():
-    return {'message': 'all blog ids provided' } 
+# @app.get('/blog/all')
+# def get_all_blogs(page = 1, page_size = 10):
+#     return {'message': f'All {page_size} blogs on page {page}'}
 
-@app.get('/blog/{id}')
-def get_blog(id: int):
-    return {'message': f"Blog with id {id}"}
+@app.get('/blog/all', tags=['blog'], 
+         summary='Get all blogs', description='This api call returns all blogs.')
+def get_all_blogs(page = 1, page_size:Optional[int] = None):
+    return {'message': f'All {page_size} blogs on page {page}'}
+
+# @app.get('/blog/{id}/comments/{comment_id}')
+# def get_comment(id:int, comment_id:int, valid:bool=True, username:Optional[str]=None):
+#     return {'message': f'blog_id {id}, comment_id {comment_id}, valid: {valid}, username: {username}'}
+@app.get('/blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
+def get_comment(id:int, comment_id:int, valid:bool=True, username:Optional[str]=None):
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid: {valid}, username: {username}'}
+
+
+# @app.get('/blog/all')
+# def get_all_blogs():
+#     return {'message': 'all blog ids provided' } 
+
+@app.get('/blog/type/{type}', tags=['blog'])
+def get_blog_type(type: BlogType):
+    return {'message': f"Blog type is {type}"}
+
+# @app.get('/blog/{id}')
+# def get_blog(id: int):
+#     return {'message': f"Blog with id {id}"}
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog {id} not found'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'message': f"Blog with id {id}"}
